c195c3e4ab9a0b070775884d5e9f0d80ce47876c
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-08-22 01:21:40 +0400

    final fix

--- pages/payment/success.js
+++ pages/payment/success.js
@@ -141,7 +141,7 @@ export default function PaymentSuccess() {
           mb={4}
           display="flex"
           alignItems="center"
-          justifyContent="center"
+          justifyContent="flex-start"
           gap={2}
         >
           <Box
@@ -160,8 +160,7 @@ export default function PaymentSuccess() {
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
             </svg>
           </Box>
-          როდესაც შეტყობინება გამოიგზავნება, გთხოვთ შეამოწმოთ თქვენი ელფოსტის
-          SPAM.
+          როდესაც შეტყობინება გამოიგზავნება, გთხოვთ შეამოწმოთ თქვენი ელფოსტის SPAM.
         </Text>
 
         <Alert status={statusInfo.status} borderRadius="md">
