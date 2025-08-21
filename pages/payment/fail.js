65d0e338b62af83366f5b789ae3aecfdf892b615
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-08-22 00:40:41 +0400

    fix

--- pages/payment/fail.js
+++ pages/payment/fail.js
@@ -15,7 +15,9 @@ export default function PaymentFail() {
       <Heading color="red.500" mb={4} fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}>
         გადახდა წარუმატებელია
       </Heading>
-      <Text mb={6} fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}>გთხოვთ, სცადეთ თავიდან ან დაუკავშირდით მხარდაჭერას.</Text>
+      <Text mb={6} fontSize={{ base: 'md', md: 'lg', lg: 'xl' }} textAlign={{ base: 'center', md: 'left' }}>
+        გთხოვთ, სცადეთ თავიდან ან დაუკავშირდით მხარდაჭერას.
+      </Text>
       <Link href="/" passHref>
         <Button colorScheme="blue">მთავარ გვერდზე დაბრუნება</Button>
       </Link>
