df5da1d76ca67c6f229d5a7cf70517a6132f430d
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-09-13 19:21:17 +0400

    add chat & update firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import AppContainer from '../components/AppContainer'
 import theme from '../config/theme'
+import { useEffect } from 'react'
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.Tawk_API = window.Tawk_API || {}
+      // window.Tawk_API.lang = 'ka';
+      const script = document.createElement('script')
+      script.async = true
+      script.src = 'https://embed.tawk.to/68b4810dd258c126a9008648/1j40hg7c5'
+      script.charset = 'UTF-8'
+      script.setAttribute('crossorigin', '*')
+      document.body.appendChild(script)
+    }
+  }, [])
   return (
     <ChakraProvider theme={theme}>
       <AppContainer>
